05d1c03908825cba4a58df5d4571fd06e9ffbb31
montoyaaa <ricardomontoya2002@hotmail.com>
2020-11-27 08:17:23 -0300

    checkup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-/* eslint-disable no-console */
-import express from 'express';
+import 'reflect-metadata';
 
+import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,35 +1,36 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-    provider: string;
     date: Date;
+    provider: string;
 }
 
 class CreateAppointmentService {
-    private appointmentsRepository: AppointmentsRepository;
+    public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(
+            AppointmentsRepository
+        );
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-    }
-
-    public execute({ date, provider }: RequestDTO): Appointment {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
             appointmentDate
         );
 
         if (findAppointmentInSameDate) {
-            throw Error('This appointment is alredy booked');
+            throw Error('This appointment is already booked');
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date: appointmentDate,
         });
+
+        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
