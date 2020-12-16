c0e720d5308cdf7386a27f07748750801557bd29
montoyaaa <ricardomontoya2002@hotmail.com>
2020-12-16 13:57:46 -0300

    add user_id in create appointments

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -9,18 +9,20 @@ import AppError from '../../../shared/errors/AppError';
 
 interface IRequest {
     provider_id: string;
+    user_id: string;
     date: Date;
 }
 
 @injectable()
 class CreateAppointmentService {
     constructor(
-        @inject('IAppointmentsRepository')
+        @inject('AppointmentsRepository')
         private appointmentsRepository: IAppointmentsRepository
     ) {}
 
     public async execute({
         date,
+        user_id,
         provider_id,
     }: IRequest): Promise<Appointment> {
         const appointmentDate = startOfHour(date);
@@ -35,6 +37,7 @@ class CreateAppointmentService {
 
         const appointment = await this.appointmentsRepository.create({
             provider_id,
+            user_id,
             date: appointmentDate,
         });
 
