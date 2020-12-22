77b7a10d941e13cb58645f153acce2107f07db81
montoyaaa <ricardomontoya2002@hotmail.com>
2020-12-22 09:59:34 -0300

    add rate-limiter-flexible

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import 'express-async-errors';
 
 import uploadConfig from '../../../config/upload';
 import AppError from '../../errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 import '../typeorm';
@@ -15,6 +16,7 @@ import '../../container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
